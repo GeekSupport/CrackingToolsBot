a83c31da1209c58759f27470bbfcd26ec1d65586
Geek Musik <106485862+GeekSupport@users.noreply.github.com>
2022-09-05 23:21:01 +0700

    Update BotConfig.py

--- BotConfig.py
+++ BotConfig.py
@@ -5,7 +5,7 @@ class Config(object):
     DL_LOCATION = os.environ.get("DL_LOCATION", "./starkgang/")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
     DEVS_EXPO = os.environ.get("DONTKANGTHISPLS", None)
-    DB_URI = os.environ.get("DATABASE_URL", None)
+    DB_URI = os.environ.get("DATABASE_URL1", None)
     API_HASH = os.environ.get("API_HASH", None)
     API_ID = int(os.environ.get("APP_ID", 6))
     LOG_CHAT = int(os.environ.get("LOG_CHAT", -111111))
